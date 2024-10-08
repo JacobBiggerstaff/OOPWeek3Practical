0c35c5fad91ac272e38b7ae32e740a27b4c6515e
Jacob Biggerstaff <Biggerstaff-J1@ulster.ac.uk>
2024-10-08 09:50:15 +0100

    Changes

--- OOPWeek3Practical/Program.cs
+++ OOPWeek3Practical/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Book
+{
+    string Title;
+    string Author;
+    string ISBN;
+
+    static void Main(string[] args)
+    {
+        //Create a new instance of the book class
+        Book book = new Book();
+        book.Title = "C# for beginners";
+        book.Author = "Bill Gates";
+        book.ISBN = " 123456677";
+
+        Book book2 = new Book();
+        book2.Title = "C# methods and classes";
+        book2.Author = "Microsoft";
+        book2.ISBN = "1234566";
+
+        //Output the book information to the console
+        Console.WriteLine($"Book title: {book.Title}");
+        Console.WriteLine($"Book author: {book.Author}");
+        Console.WriteLine($"Book ISBN: {book.ISBN}");
+
+        Console.WriteLine($"Book2 title: {book2.Title}");
+        Console.WriteLine($"Book2 author: {book2.Author}");
+        Console.WriteLine($"Book2 ISBN: {book2.ISBN}");
+
+        //Output
+    }
+}
