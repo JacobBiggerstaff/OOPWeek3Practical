eaed480f77a294b05806e56e41a69659bad81942
Jacob Biggerstaff <Biggerstaff-J1@ulster.ac.uk>
2024-10-08 10:09:53 +0100

    changes again

--- OOPWeek3Practical/Program.cs
+++ OOPWeek3Practical/Program.cs
@@ -3,6 +3,20 @@
     string Title;
     string Author;
     string ISBN;
+    int NoOfPages;
+
+
+    void DisplayInfo()
+    {
+        //Output the book information to the console
+        Console.WriteLine($"Book Information");
+        Console.WriteLine($"----------------");
+        Console.WriteLine($"Book title: {Title}");
+        Console.WriteLine($"Book author: {Author}");
+        Console.WriteLine($"Book ISBN: {ISBN}");
+        Console.WriteLine($"No of pages: {NoOfPages}");
+        Console.WriteLine();
+    }
 
     static void Main(string[] args)
     {
@@ -11,20 +25,20 @@
         book.Title = "C# for beginners";
         book.Author = "Bill Gates";
         book.ISBN = " 123456677";
+        book.NoOfPages = 200;
 
         Book book2 = new Book();
         book2.Title = "C# methods and classes";
         book2.Author = "Microsoft";
         book2.ISBN = "1234566";
+        book2.NoOfPages = 50;
 
-        //Output the book information to the console
-        Console.WriteLine($"Book title: {book.Title}");
-        Console.WriteLine($"Book author: {book.Author}");
-        Console.WriteLine($"Book ISBN: {book.ISBN}");
+        // Output book information to the console
 
-        Console.WriteLine($"Book2 title: {book2.Title}");
-        Console.WriteLine($"Book2 author: {book2.Author}");
-        Console.WriteLine($"Book2 ISBN: {book2.ISBN}");
+
+
+        book.DisplayInfo();
+        book2.DisplayInfo();
 
         //Output
     }
